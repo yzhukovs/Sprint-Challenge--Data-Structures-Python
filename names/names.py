b160aace65bd3650f4b5652b470e42bdb38361e3
yzhukovs <yzhukovs@bumnetworks.com>
2019-04-05 12:12:43 -0400

    improved runtime to:  0.004784107208251953 seconds

--- names/names.py
+++ names/names.py
@@ -16,15 +16,20 @@ duplicates = []
 #         if name_1 == name_2:
 #             duplicates.append(name_1)
 
-lookup_names = {} #using dictionary 
-for name_1 in names_1:  # O(n)
-    lookup_names[name_1] = name_1	# O(1)
+#USING DICTIONARY AND LOOP------------
+# lookup_names = {} #using dictionary 
+# for name_1 in names_1:  # O(n)
+#     lookup_names[name_1] = name_1	# O(1)
     
-for name_2 in names_2:  # 0(n)
-    if name_2 in lookup_names:  # lookup in dictionary is O(1)
-        duplicates.append(lookup_names[name_2])
+# for name_2 in names_2:  # 0(n)
+#     if name_2 in lookup_names:  # lookup in dictionary is O(1)
+#         duplicates.append(lookup_names[name_2])
 
-#runtime: 0.005610942840576172 seconds
+# #runtime: 0.005610942840576172 seconds
+
+#USING SET() ------------------------
+duplicates = set(names_1) & set(names_2)
+#runtime: 0.004784107208251953 seconds
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
