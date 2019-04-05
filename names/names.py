364f91f9d074c26585933276172348991f8a21c9
yzhukovs <yzhukovs@bumnetworks.com>
2019-04-05 12:05:46 -0400

    improved runtime from: 7.575293064117432 seconds to runtime: 0.005610942840576172 seconds

--- names/names.py
+++ names/names.py
@@ -11,10 +11,20 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:   #runtime: 7.575293064117432 seconds
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+lookup_names = {} #using dictionary 
+for name_1 in names_1:  # O(n)
+    lookup_names[name_1] = name_1	# O(1)
+    
+for name_2 in names_2:  # 0(n)
+    if name_2 in lookup_names:  # lookup in dictionary is O(1)
+        duplicates.append(lookup_names[name_2])
+
+#runtime: 0.005610942840576172 seconds
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
